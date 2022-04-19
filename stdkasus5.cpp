f519bef5637db8ace17418dc3df98bcd86c6feb1
Muhammad Khysam F <101845963+Khysam@users.noreply.github.com>
2022-04-20 04:35:55 +0700

    Update stdkasus5.cpp

--- stdkasus5.cpp
+++ stdkasus5.cpp
@@ -20,3 +20,25 @@ void proses::hitung(){
   
   proses p;
   cout << "Masukkan NIM Mahasiswa : ";
+  cin >> nim;
+  cout << "Masukkan Nama Mahasiswa : ";
+  cin >> nama;
+  cout << "Masukkan jumlah matakuliah yang mau diambil :";
+  cin>>jml_matkul;
+  hasil=p.ambil_matkul(jml_matkul);
+  total = hasil * biaya;
+  diskon = total * 10/100;
+  total_biaya1 = total - diskon;
+  total_diskon = total_biaya1 * 5/100;
+  total_biaya2 = total_biaya1 - total_diskon;
+  cout <<"\n\n---------------------------------";
+  cout << "\nNIM Mahasiswa              : " << nim;
+  cout << "\nNama Mahasiswa             : " << nama;
+  cout << "\nJumlah matakuliah yang diambil   : " << jml_matkul;
+  cout << "\nJumlah sks yang diambil        : " << hasil;
+  cout << "\nTotal biaya              : " << total << endl ;
+  cout << "Total biaya setelah di diskon    : " << total_biaya2;
+}
+int proses::ambil_matkul(int x){
+  if (x==0){
+    cout << "Jumlah keseluruhan sks : " << hasil;
